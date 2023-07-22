315ebca0954efe3fd147aabb8acb28255e6f3070
Javeriya <javeriya.noorisys@gmail.com>
2023-07-22 16:13:14 +0530

    bug

--- resources/views/landing_page/footer.blade.php
+++ resources/views/landing_page/footer.blade.php
@@ -44,4 +44,4 @@
 
 
     <!-- Back to Top -->
-    <a href="#" class="btn btn-lg btn-lg-square back-to-top pt-2"><i class="bi bi-arrow-up text-white"></i></a>
+    <a href="#" class="btn btn-lg btn-lg-square back-to-top pt-2"><b><i class="bi bi-arrow-up text-white"></i></b></a>
