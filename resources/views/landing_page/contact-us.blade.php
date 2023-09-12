d65d9fa723003984161593d3d85e7d6666013c63
Javeriya <javeriya.noorisys@gmail.com>
2023-09-12 15:53:04 +0530

    bug

--- resources/views/landing_page/contact-us.blade.php
+++ resources/views/landing_page/contact-us.blade.php
@@ -1,4 +1,13 @@
- 
+ <style>
+    .heading {
+        max-width: 1000px;
+        margin: 0 auto;
+        padding: 20px;
+        text-align: center;
+        margin-left: 50%;
+        margin-top: 20%;
+    }
+ </style>
  <!-- Screenshot Start -->
  <div class="faq_area section_padding_140" id="faq" style="min-height: 80vh;">
     <div class="container">
@@ -6,11 +15,11 @@
 
             <div class="col-12 col-sm-8 col-lg-8">
                 <!-- Section Heading-->
-                <div class="section_heading wow fadeInUp pb-5" data-wow-delay="0.2s"
+                <div class="section_heading wow fadeInUp pb-5 heading" data-wow-delay="0.2s"
                     style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
                     <img style="width: 300px; height: auto; padding-bottom: 20px;" src="assets/img/Screenshot_2023-09-11_200248-removebg-preview.png" alt="">
-                    <h3>Contact  Us</h3>
-                    <p>For any inquiries or support, please feel free to contact us at contact@qysmat.com. <br> We are always here to help and will aim to respond within 24-48 hours.
+                    <h1>Contact Us</h1>
+                    <p>For any inquiries or support, please feel free to contact us at <a href="contact@qysmat.com">contact@qysmat.com</a>. We are always here to help and will aim to respond within 24-48 hours.</p>
                     </p>
                     <!-- <p>Appland is completely creative, lightweight, clean &amp; super responsive app landing page.</p> -->
                     <div class="line"></div>
