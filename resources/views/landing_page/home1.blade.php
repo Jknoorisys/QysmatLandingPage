4b110676b8ac4267b0c354736fce09e3067beb23
Javeriya <javeriya.noorisys@gmail.com>
2023-09-12 09:50:09 +0530

    Landing page

---
+++ resources/views/landing_page/home1.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Coming Soon</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin: 100px;
+        }
+
+        h1 {
+            font-size: 36px;
+        }
+
+        p {
+            font-size: 18px;
+        }
+    </style>
+</head>
+<body>
+    <h1 class="mt-4">Coming Soon!</h1>
+    <p>We are currently performing maintenance on the website.</p>
+    <p>Please check back soon. Thank you for your patience!</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -18,25 +18,25 @@ Route::get('/', function () {
     $data['installs'] = DB::table('web_pages')->where('page_name', 'installs')->first();
     $data['reviews'] = DB::table('web_pages')->where('page_name', 'reviews')->first();
 
-    $data['content'] = view('landing_page.home', $data);
+    $data['content'] = view('landing_page.home1', $data);
     return view('landing_page.main1', $data);
 })->name('/');
 
 Route::get('privacy-policy', function () {
     $data['page'] = DB::table('web_pages')->where('page_name', '=', 'privacy_policy')->first();
     $data['content'] = view('landing_page.privacypolicy', $data);
-    return view('landing_page.main', $data);
+    return view('landing_page.main1', $data);
 })->name('privacy-policy');
 
 Route::get('terms-conditions', function () {
     $data['page'] = DB::table('web_pages')->where('page_name', '=', 'terms_and_conditions')->first();
     $data['content'] = view('landing_page.terms&condition', $data);
-    return view('landing_page.main', $data);
+    return view('landing_page.main1', $data);
 })->name('terms-conditions');
 
 Route::get('faqs', function () {
     $data['faqs'] = DB::table('faqs')->where('status', '=', 'Active')->get();
     $data['content'] = view('landing_page.faq', $data);
-    return view('landing_page.main', $data);
+    return view('landing_page.main1', $data);
 })->name('faqs');
 
