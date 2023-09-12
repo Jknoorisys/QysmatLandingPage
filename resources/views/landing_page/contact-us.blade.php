0960e1a3462dc4ae3c8754786e466c48c7937a99
Javeriya <javeriya.noorisys@gmail.com>
2023-09-12 15:38:20 +0530

    bug

--- resources/views/landing_page/contact-us.blade.php
+++ resources/views/landing_page/contact-us.blade.php
@@ -1,44 +1,5 @@
-<style>
-    body {
-        background-color: var(--primary);
-        color: rgb(0, 0, 0);
-        font-family: Arial, sans-serif;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        margin: 0;
-    }
-
-    .container {
-        background-color: var(--light);
-        border-radius: 10px;
-        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-        padding: 20px;
-        width: 300px;
-        text-align: center;
-    }
-
-    h1 {
-        color: var(--secondary);
-    }
-
-    p {
-        color: var(--dark);
-    }
-
-    a {
-        color: var(--secondary);
-        text-decoration: none;
-    }
-
-    a:hover {
-        text-decoration: underline;
-    }
-</style>
-
  <!-- Screenshot Start -->
- <div class="faq_area section_padding_140" id="faq" style="min-height: 80vh;">
+ <div class="faq_area section_padding_140" id="faq" style="min-height: 5vh;">
     <div class="container">
         <div class="row ">
 
@@ -47,14 +8,9 @@
                 <div class="section_heading wow fadeInUp pb-5" data-wow-delay="0.2s"
                     style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
                     <img style="width: 300px; height: auto; padding-bottom: 20px;" src="assets/img/Screenshot_2023-09-11_200248-removebg-preview.png" alt="">
-                    {{-- <h3>Contact  Us</h3>
+                    <h3>Contact  Us</h3>
                     <p>For any inquiries or support, please feel free to contact us at contact@qysmat.com. <br> We are always here to help and will aim to respond within 24-48 hours.
-                    </p> --}}
-                    <div class="container">
-                        <h1>Contact Us</h1>
-                        <p>For any inquiries or support, please feel free to contact us at <a href="mailto:contact@qysmat.com">contact@qysmat.com</a>.</p>
-                        <p>We are always here to help and will aim to respond within 24-48 hours.</p>
-                    </div>
+                    </p>
                     <!-- <p>Appland is completely creative, lightweight, clean &amp; super responsive app landing page.</p> -->
                     <div class="line"></div>
                 </div>
