cbebdceb8d2551f83b914f678a20dd6017dd7847
Javeriya <javeriya.noorisys@gmail.com>
2023-09-15 12:26:56 +0530

    footer

--- resources/views/landing_page/footer.blade.php
+++ resources/views/landing_page/footer.blade.php
@@ -37,7 +37,7 @@
                         <a href="{{ route('privacy-policy') }}" class="text-white">Privacy Policy</a>
                     </div> --}}
                     <div class="footer-menu">
-                        <a href="{{ route('/') }}" class="text-white">FAQs</a>
+                        <a href="{{ route('faqs') }}" class="text-white">FAQs</a>
                         <a href="{{ route('contact-us') }}" class="text-white">Contact Us</a>
                         <a href="{{ route('/') }}" class="text-white">Terms & Conditions</a>
                         <a href="{{ route('privacy-policy') }}" class="text-white">Privacy Policy</a>
