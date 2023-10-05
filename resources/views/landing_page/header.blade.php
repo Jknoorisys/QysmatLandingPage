43b95333dd175b403bc69f0ef5e5550ac5a1e533
Javeriya <javeriya.noorisys@gmail.com>
2023-10-05 17:33:59 +0530

    bug

--- resources/views/landing_page/header.blade.php
+++ resources/views/landing_page/header.blade.php
@@ -7,70 +7,56 @@
 <!-- Spinner End -->
 
 <!-- Navbar & Hero Start -->
-<div class="container-xxl position-relative p-0" id="home">
-    <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
-        <a href="/" class="navbar-brand pt-2" style="z-index: 100;  padding: 0 10px 0 10px; border-bottom-left-radius: 6px; border-bottom-right-radius: 6px;">
-            <img src="assets/img/qysmat-logo.png" alt="Logo">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" style="color: #8F7C5C;">
-            <span class="fa fa-bars"></span>
-        </button>
-    </nav>
 
-    <div class="container-xxl bg-primary hero-header">
+<div class="container-xxl hero-header">
 
-        <video autoplay muted loop id="myVideo">
-            <source src="assets/img/Qysmat-desktop.mp4" type="video/mp4">
-            Your browser does not support HTML5 video.
-        </video>
-        <video autoplay muted loop id="myVideo2">
-            <source src="assets/img/Qysmat_video_2.mp4" type="video/mp4">
-            Your browser does not support HTML5 video.
-        </video>
+    <video autoplay muted loop id="myVideo">
+        <source src="img/Qysmat-desktop.mp4" type="video/mp4">
+        Your browser does not support HTML5 video.
+    </video>
+    <video autoplay muted loop id="myVideo2">
+        <source src="img/Qysmat_video_2.mp4" type="video/mp4">
+        Your browser does not support HTML5 video.
+    </video>
 
-        <div class="container mobile-version px-lg-5">
-            <div class="row g-5">
-                <div class="col-md-7 overlay-text text-center text-lg-start pb-5">
-                    <h2 class="text-white mb-4 animated slideInDown"
-                        style="text-shadow: 3px 4px 7px rgba(15, 12, 1, 0.8)">FIND YOUR QYSMAT
-                    </h2>
-                    <p class="text-white pb-3 animated slideInDown" style="font-size: 25px;text-shadow: 3px 4px 7px rgba(15, 12, 1, 0.8)">First FREE app for Parents to help Single Muslims find marriage & search together
-                    </p>
-                  
-                    <div class="row g-4">
-                        <div class="col-sm-6 col-lg-4 col-6 wow fadeIn" data-wow-delay="0.5s"
-                            style="z-index: 100;">
-                            <a href="https://apps.apple.com/us/app/umrahplus/id1640429513" class="d-flex bg-primary-gradient rounded py-3 px-3">
-                                <i class="fab fa-apple fa-3x text-white flex-shrink-0"></i>
-                                <div class="ms-3 app-store">
-                                    <p class="text-white mb-0">Available On</p>
-                                    <h5 class="text-white mb-0">App Store</h5>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-sm-6 col-lg-4 col-6 wow fadeIn" data-wow-delay="0.7s"
-                            style="z-index: 100;">
-                            <a href="https://play.google.com/store/apps/details?id=com.app.umacustomer" class="d-flex bg-secondary-gradient rounded py-3 px-3">
-                                <i class="fab fa-android fa-3x text-white flex-shrink-0"></i>
-                                <div class="ms-3 google-play">
-                                    <p class="text-white mb-0">Available On</p>
-                                    <h5 class="text-white mb-0">Play Store</h5>
-                                </div>
-                            </a>
-                        </div>
+    <div class="container mobile-version px-lg-5">
+        <div class="row g-5">
+            <div class="col-md-7 overlay-text text-center text-lg-start pb-5">
+                <h2 class="text-white mb-4 animated slideInDown"
+                    style="text-shadow: 3px 4px 7px rgba(15, 12, 1, 0.8)">FIND YOUR QYSMAT
+                </h2>
+                <p class="text-white pb-3 animated slideInDown" style="font-size: 25px;text-shadow: 3px 4px 7px rgba(15, 12, 1, 0.8)">First FREE app for Parents to help Single Muslims find marriage & search together
+                </p>
+                <!-- <a href="" class="btn btn-primary-gradient py-sm-3 px-4 px-sm-5 rounded-pill me-3 animated slideInLeft">Read More</a>
+                <a href="" class="btn btn-secondary-gradient py-sm-3 px-4 px-sm-5 rounded-pill animated slideInRight">Contact Us</a> -->
+                <div class="row g-4">
+                    <div class="col-sm-6 col-lg-4 col-6 wow fadeIn" data-wow-delay="0.5s"
+                        style="z-index: 100;">
+                        <a href="https://apps.apple.com/us/app/umrahplus/id1640429513" class="d-flex bg-primary-gradient rounded py-3 px-3">
+                            <i class="fab fa-apple fa-3x text-white flex-shrink-0"></i>
+                            <div class="ms-3 app-store">
+                                <p class="text-white mb-0">Available On</p>
+                                <h5 class="text-white mb-0">App Store</h5>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-sm-6 col-lg-4 col-6 wow fadeIn" data-wow-delay="0.7s"
+                        style="z-index: 100;">
+                        <a href="https://play.google.com/store/apps/details?id=com.app.umacustomer" class="d-flex bg-secondary-gradient rounded py-3 px-3">
+                            <i class="fab fa-android fa-3x text-white flex-shrink-0"></i>
+                            <div class="ms-3 google-play">
+                                <p class="text-white mb-0">Available On</p>
+                                <h5 class="text-white mb-0">Play Store</h5>
+                            </div>
+                        </a>
                     </div>
                 </div>
-                <div class="col-md-5 cutomeHeight">
-                    <!-- <div class="owl-carousel screenshot-carousel">
-                        <img class="img-fluid" src="img/screen-1.png" alt="">?
-                        <img class="img-fluid" src="img/1_Splash_screen_3.png?" alt="">
-                        <img class="img-fluid" src="img/screen-3.png" alt="">?
-                        <img class="img-fluid" src="img/screen-4.png" alt="">?
-                    </div> -->
-                </div>
+            </div>
+            <div class="col-md-5 cutomeHeight">
+               
             </div>
         </div>
     </div>
 </div>
-<!-- Navbar & Hero End -->
 
+<!-- Navbar & Hero End -->
