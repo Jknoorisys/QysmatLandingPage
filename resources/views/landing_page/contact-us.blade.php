c3b1de82d1d8e8f8949c188ddd7b5b2727613faa
Javeriya <javeriya.noorisys@gmail.com>
2023-10-05 11:53:34 +0530

    bug

--- resources/views/landing_page/contact-us.blade.php
+++ resources/views/landing_page/contact-us.blade.php
@@ -1,26 +1,134 @@
- <style>
-    .heading {
-        max-width: 1000px;
-        margin: 0 auto;
-        padding: 20px;
-        text-align: center;
-        margin-left: 50%;
-        margin-top: 20%;
+<style>
+    .section_padding_140 {
+        padding-top: 110px;
+        padding-bottom: 110px;
     }
- </style>
+
+    .faq_area {
+        position: relative;
+        z-index: 1;
+        /* background-color: #f5f5ff; */
+    }
+
+    .logo img {
+        height: 100px;
+        width: auto;
+    }
+
+    .faq-accordian {
+        position: relative;
+        z-index: 1;
+    }
+
+    .faq-accordian .card {
+        position: relative;
+        z-index: 1;
+        margin-bottom: 1.5rem;
+    }
+
+    .faq-accordian .card:last-child {
+        margin-bottom: 0;
+    }
+
+    .faq-accordian .card .card-header {
+        background-color: #ffffff;
+        padding: 0;
+        border-bottom-color: #ebebeb;
+
+    }
+
+    .faq-accordian .card .card-header h6 {
+        cursor: pointer;
+        padding: 1.75rem 2rem;
+        color: #3f43fd;
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        -ms-grid-row-align: center;
+        align-items: center;
+        -webkit-box-pack: justify;
+        -ms-flex-pack: justify;
+        justify-content: space-between;
+    }
+
+    .faq-accordian .card .card-header h6 span {
+        font-size: 1.5rem;
+    }
+
+    .faq-accordian .card .card-header h6.collapsed {
+        color: #070a57;
+        border-left: 4px solid #4B371C;
+    }
+
+    .faq-accordian .card .card-header h6.collapsed span {
+        -webkit-transform: rotate(-180deg);
+        transform: rotate(-180deg);
+    }
+
+    .faq-accordian .card .card-body {
+        padding: 1.75rem 2rem;
+    }
+
+    .faq-accordian .card .card-body p:last-child {
+        margin-bottom: 0;
+    }
+
+    .faq_area h1 {
+        color: #4B371C;
+    }
+
+    .faq_area h1 span {
+        background: #fea501;
+        color: #fff;
+        padding: 0 10px 0 10px;
+        /* font-size: 30px; */
+        border-radius: 10px;
+    }
+
+    @media only screen and (max-width: 575px) {
+        .support-button p {
+            font-size: 14px;
+        }
+    }
+
+    .support-button i {
+        color: #3f43fd;
+        font-size: 1.25rem;
+    }
+
+    @media only screen and (max-width: 575px) {
+        .support-button i {
+            font-size: 1rem;
+        }
+    }
+
+    .support-button a {
+        text-transform: capitalize;
+        color: #2ecc71;
+    }
+
+    @media only screen and (max-width: 575px) {
+        .support-button a {
+            font-size: 13px;
+        }
+    }
+</style>
  <!-- Screenshot Start -->
- <div class="faq_area section_padding_140" id="faq" style="min-height: 80vh;">
+ <div class="faq_area section_padding_140" id="faq" style="min-height: 5vh;">
     <div class="container">
         <div class="row ">
 
             <div class="col-12 col-sm-8 col-lg-8">
                 <!-- Section Heading-->
-                <div class="section_heading wow fadeInUp pb-5 heading" data-wow-delay="0.2s"
-                <div class="section_heading wow fadeInUp pb-5 heading" data-wow-delay="0.2s"
+                <div class="section_heading wow fadeInUp pb-5" data-wow-delay="0.2s"
                     style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
-                    <img style="width: 300px; height: auto; padding-bottom: 20px;" src="assets/img/Screenshot_2023-09-11_200248-removebg-preview.png" alt="">
-                    <h1>Contact Us</h1>
-                    <p>For any inquiries or support, please feel free to contact us at <a href="contact@qysmat.com">contact@qysmat.com</a>. We are always here to help and will aim to respond within 24-48 hours.</p>
+                    <img style="width: 300px; height: auto; padding-bottom: 20px;" src="img/Screenshot_2023-09-11_200248-removebg-preview.png" alt="">
+                    <h3>Contact  Us</h3>
+                    <p>For any inquiries or support, please feel free to contact us at contact@qysmat.com. <br> We are always here to help and will aim to respond within 24-48 hours.
+
+
                     </p>
                     <!-- <p>Appland is completely creative, lightweight, clean &amp; super responsive app landing page.</p> -->
                     <div class="line"></div>
