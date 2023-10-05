09c9a2ff0a85553df6a9e1e1ec484108e6ab6d30
Javeriya <javeriya.noorisys@gmail.com>
2023-10-05 17:50:17 +0530

    bug

--- resources/views/landing_page/navbar.blade.php
+++ resources/views/landing_page/navbar.blade.php
@@ -1,7 +1,7 @@
 <!-- Navbar & Hero Start -->
 <div class="container-xxl position-relative p-0" id="home">
     <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
-        <a href="" class="navbar-brand pt-2"
+        <a href="{{ route('/') }}" class="navbar-brand pt-2"
             style="z-index: 100;  padding: 0 10px 0 10px; border-bottom-left-radius: 6px; border-bottom-right-radius: 6px;">
 
             <img src="assets/img/qysmat-logo.png" alt="Logo">
